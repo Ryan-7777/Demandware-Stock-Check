65fbc7898e18d01e8f843c41d6e010cca6038198
Ryan-7777 <ryan@rampageproxies.com>
2020-10-30 14:34:55 +0000

    Update demandware-stock.py

--- demandware-stock.py
+++ demandware-stock.py
@@ -3,8 +3,8 @@ import atexit
 import json
 import time
 
-product_sku = 'FX9846'
-site = 'yeezysupply'  # Any demandware site (i.e. yeezysupply or adidas)
+product_sku = ''
+site = 'adidas'  # Any demandware site (i.e. yeezysupply or adidas)
 target_site = 'https://www.' + str(site).lower() + '.com/api/products/' + str(product_sku).upper() + '/availability'
 timeout_retry_seconds = 180
 refresh_rate_seconds = 15
